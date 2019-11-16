9e89e96dd3cd24bbf79c07e330a1467cae6a793a
Marcin <m.nowak@pleodigital.com>
2019-11-16 14:39:52 +0100

    Updates

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `Nowa wersja aplikacji została pobrana. ` +
-    `Czy chcesz przeładować?`
-  )
-  if (answer === true) {
-    window.location.reload()
-  }
-}
