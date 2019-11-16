bc79544d39196827b523c58cfa31201d8d6e7dc0
Marcin <m.nowak@pleodigital.com>
2019-11-16 14:37:13 +0100

    up

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,9 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `Nowa wersja aplikacji została pobrana. ` +
+    `Czy chcesz przeładować?`
+  )
+  if (answer === true) {
+    window.location.reload()
+  }
+}
